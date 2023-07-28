2e9c57872f42eb50857f5f46af472f5babef9c55
Daniil Siamenka <maloigb@gmail.com>
2023-07-28 22:38:02 +0300

    addAxios

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
